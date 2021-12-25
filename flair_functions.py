335f2d9c2ef6fa3a0b2b7fdec612428fb0ab646e
isFakeAccount <trevorphillips@gmx.us>
2021-12-25 01:04:00 -0500

    Added logging, updated comment, and improved code

--- flair_functions.py
+++ flair_functions.py
@@ -1,19 +1,27 @@
-import json
+import logging
 
-import praw
+import yaml
 
 import CONSTANTS
 import bot_responses
 
+flair_func_logger = logging.getLogger('main')
 
-# Checks if the author is mod
+
 def is_mod_or_courier(author, fallout76marketplace):
+    """
+    Checks if the author is mod.
+
+    :param author: The redditor which will be checked.
+    :param fallout76marketplace: The subreddit in which the moderator/courier position will be checked.
+    :return: True if the user is courier/moderator otherwise False
+    """
     if author is None:
         return False
     moderators_list = fallout76marketplace.moderator()
-    wiki = fallout76marketplace.wiki["custom_bot_config"]
-    json_format = json.loads(wiki.content_md)
-    courier_list = json_format['couriers']
+    wiki = fallout76marketplace.wiki["custom_bot_config/courier_list"]
+    yaml_format = yaml.safe_load(wiki.content_md)
+    courier_list = [x.lower() for x in yaml_format['couriers']]
     if author in moderators_list:
         return True
     if author.name.lower() in courier_list:
@@ -21,72 +29,90 @@ def is_mod_or_courier(author, fallout76marketplace):
     return False
 
 
-# Assigns flair to user based on karma value and mod status
 def assign_karma(p_comment, user_flair, fallout76marketplace):
+    """
+    Assigns flair to user based on karma value and mod/courier status.
+
+    :param p_comment: The comment whose author flair will be updated.
+    :param user_flair: The updated user flair text.
+    :param fallout76marketplace: The subreddit in which the flair will be updated.
+    :return: None
+    """
     author_name = p_comment.author.name
     # Splits Flair into two
     user_flair_split = user_flair.split()
 
     # If user is mod assigns the green flair
     if is_mod_or_courier(p_comment.author, fallout76marketplace):
-        fallout76marketplace.flair.set(author_name, text=str(user_flair),
-                                       flair_template_id=CONSTANTS.MODS_AND_COURIERS_FLAIR)
+        fallout76marketplace.flair.set(author_name, text=user_flair, flair_template_id=CONSTANTS.MODS_AND_COURIERS_FLAIR)
     else:
-        # otherwise assigns flair based on karma value
+        # otherwise, assigns flair based on karma value
         karma_value = int(user_flair_split[-1])
         if karma_value < 49:
-            fallout76marketplace.flair.set(author_name, text=str(user_flair),
-                                           flair_template_id=CONSTANTS.ZERO_TO_FIFTY_FLAIR)
+            fallout76marketplace.flair.set(author_name, text=user_flair, flair_template_id=CONSTANTS.ZERO_TO_FIFTY_FLAIR)
         elif 50 <= karma_value < 99:
-            fallout76marketplace.flair.set(author_name, text=str(user_flair),
-                                           flair_template_id=CONSTANTS.FIFTY_TO_HUNDRED_FLAIR)
+            fallout76marketplace.flair.set(author_name, text=user_flair, flair_template_id=CONSTANTS.FIFTY_TO_HUNDRED_FLAIR)
         else:
-            fallout76marketplace.flair.set(author_name, text=str(user_flair),
-                                           flair_template_id=CONSTANTS.ABOVE_HUNDRED_FLAIR)
+            fallout76marketplace.flair.set(author_name, text=user_flair, flair_template_id=CONSTANTS.ABOVE_HUNDRED_FLAIR)
+    flair_func_logger.info(f"Updated the user flair for {author_name} to {user_flair}")
 
 
-# Increments karma by 1
 def increment_karma(comment, fallout76marketplace):
+    """
+    Increments the karma of parent comment author.
+
+    :param comment: Comment that triggered the command.
+    :param fallout76marketplace: Subreddit instance.
+    :return: None
+    """
     try:
         p_comment = comment.parent()
         author_name = p_comment.author.name
     except AttributeError:
         bot_responses.karma_reward_failed(comment)
         return -1
+
     # if the author has no flair
-    if p_comment.author_flair_css_class == '' or p_comment.author_flair_css_class is None:
+    if not p_comment.author_flair_css_class:
         # sets the flair to one
-        fallout76marketplace.flair.set(author_name, text='Karma: 1',
-                                       flair_template_id=CONSTANTS.ZERO_TO_FIFTY_FLAIR)
+        fallout76marketplace.flair.set(author_name, text='Karma: 1', flair_template_id=CONSTANTS.ZERO_TO_FIFTY_FLAIR)
+        flair_func_logger.info(f"Setting the flair of u/{author_name} to Karma: 1.")
     else:
         # Getting the flair and adding the value
         user_flair = p_comment.author_flair_text
-
         # Splits Karma into two
         user_flair_split = user_flair.split()
         try:
             user_flair_split[-1] = int(user_flair_split[-1])
             user_flair_split[-1] += 1
+            flair_func_logger.info(f"Incremented the karma of u/{author_name} to {user_flair_split[-1]}.")
         except ValueError:
-            pass
+            flair_func_logger.warning(f"The karma value for user u/{author_name} with flair {user_flair} could not be incremented.")
         # Combines back string and int part
         user_flair = ' '.join(map(str, user_flair_split))
         assign_karma(p_comment, user_flair, fallout76marketplace)
 
 
-# Decrements karma by 1
 def decrement_karma(comment, fallout76marketplace):
+    """
+    Decrements the karma of parent comment author.
+
+    :param comment: Comment that triggered the command.
+    :param fallout76marketplace: Subreddit instance.
+    :return: None
+    """
     try:
         p_comment = comment.parent()
         author_name = p_comment.author.name
     except AttributeError:
         bot_responses.karma_reward_failed(comment)
         return -1
+
     # if the author has no flair
-    if p_comment.author_flair_css_class == '' or p_comment.author_flair_css_class is None:
+    if not p_comment.author_flair_css_class:
         # sets the flair to one
-        fallout76marketplace.flair.set(author_name, text='Karma: -1',
-                                       flair_template_id=CONSTANTS.ZERO_TO_FIFTY_FLAIR)
+        fallout76marketplace.flair.set(author_name, text='Karma: -1', flair_template_id=CONSTANTS.ZERO_TO_FIFTY_FLAIR)
+        flair_func_logger.info(f"Setting the flair of u/{author_name} to Karma: -1.")
     else:
         # Getting the flair and adding the value
         user_flair = p_comment.author_flair_text
@@ -96,18 +122,22 @@ def decrement_karma(comment, fallout76marketplace):
         try:
             user_flair_split[-1] = int(user_flair_split[-1])
             user_flair_split[-1] -= 1
+            flair_func_logger.info(f"Decremented the karma of u/{author_name} to {user_flair_split[-1]}.")
         except ValueError:
-            pass
+            flair_func_logger.warning(f"The karma value for user u/{author_name} with flair {user_flair} could not be decremented.")
         # Combines back string and int part
         user_flair = ' '.join(map(str, user_flair_split))
         assign_karma(p_comment, user_flair, fallout76marketplace)
 
 
-# Changes the flair to Trade Closed and locks submission
-def close_post_trade(comment_or_submission):
-    if isinstance(comment_or_submission, praw.models.reddit.submission.Submission):
-        submission = comment_or_submission
-    else:
-        submission = comment_or_submission.submission
+def close_post_trade(comment):
+    """
+    Changes the flair to Trade Closed and locks submission.
+
+    :param comment: Comment that triggered the command.
+    :return:
+    """
+    submission = comment.submission
     submission.flair.select(CONSTANTS.TRADE_ENDED_ID)
     submission.mod.lock()
+    flair_func_logger.info(f"Closed the submission with id {submission.id}")
