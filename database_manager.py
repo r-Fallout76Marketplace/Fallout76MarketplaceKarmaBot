756e1a2775293f40afc55edb150064e3efecb11c
isFakeAccount <trevorphillips@gmx.us>
2021-12-25 01:50:15 -0500

    Added logging for the user commands

--- database_manager.py
+++ database_manager.py
@@ -40,44 +40,38 @@ def karma_plus_command_non_mod_users(comment, fallout76marketplace, legacy76, db
     :return: returns the int value based on checks
 
     **PASS CODES**
-
     KARMA_CHECKS_PASSED = 0 (If all the checks passed)
 
     **FAIL CODES**
-
     CANNOT_REWARD_YOURSELF = 1 (If the user tried to reward themselves)
-
     MORE_THAN_TWO_USERS = 2 (If there are more than 2 users involved in the conversation)
-
     CONVERSATION_NOT_LONG_ENOUGH = 3 (If the conversation is not long enough)
-
     INCORRECT_SUBMISSION_TYPE = 4 (If the flair of submission is incorrect)
-
     ALREADY_REWARDED = 5 (If the user already rewarded that particular user in the submission)
-
     KARMA_AWARDING_LIMIT_REACHED = 6 (If user have reached the limit they can give karma)
-
     DELETED_OR_REMOVED = 7 (If the user tried to give karma to removed commented/submission)
     """
     # Checks if we have the submission in database otherwise assumes that the flair was changed
     submission = comment.submission
     if not common_functions.flair_checks(comment):
+        db_manager_logger.info("Flair checks flair for the karma command.")
         bot_responses.karma_trading_posts_only(comment)
         return CONSTANTS.INCORRECT_SUBMISSION_TYPE
 
     # User tries to give karma to deleted parent obj
     if comment.parent().author is None:
+        db_manager_logger.info("Parent comment deleted therefore karma cannot be given.")
         bot_responses.deleted_or_removed(comment)
         return CONSTANTS.DELETED_OR_REMOVED
 
     with closing(db_conn.cursor()) as cursor:
         # Checks in karma logs to see if user has already rewarded the user
-        cursor.execute(f"""SELECT * FROM comments WHERE submission_id='{submission.id}' 
-                                                    AND from_author_name='{comment.author.name}'
-                                                    AND to_author_name='{comment.parent().author.name}'""")
+        cursor.execute(f"""SELECT * FROM comments WHERE submission_id='{submission.id}' AND from_author_name='{comment.author.name}' 
+                                                                                        AND to_author_name='{comment.parent().author.name}'""")
         result = cursor.fetchone()
 
     if result is not None:
+        db_manager_logger.info(f"{comment.author.name} has already reward to {comment.parent().author.name} at comment {result[0]}")
         bot_responses.already_rewarded_comment(comment, result[6])
         return CONSTANTS.ALREADY_REWARDED
 
@@ -87,22 +81,23 @@ def karma_plus_command_non_mod_users(comment, fallout76marketplace, legacy76, db
         seconds_from_previous_midnight = time.localtime().tm_hour * 3600 + time.localtime().tm_min * 60 + time.localtime().tm_sec
         unix_time_at_previous_midnight = time_now - seconds_from_previous_midnight
 
-        cursor.execute(f"""SELECT * FROM comments WHERE from_author_name='{comment.author.name}' AND 
-        time_created_utc >= '{unix_time_at_previous_midnight}'""")
+        cursor.execute(f"""SELECT * FROM comments WHERE from_author_name='{comment.author.name}' AND time_created_utc >= '{unix_time_at_previous_midnight}'""")
         result = cursor.fetchall()
 
         if len(result) >= 10:
+            db_manager_logger.info(f"u/{comment.author.name} have reached their awarder karma limit.")
             link = bot_responses.karma_reward_limit_reached(comment, result, legacy76)
             try:
-                common_functions.send_message_to_discord(mod_channel_webhook,
-                                                         f"{comment.author.name} has reached awarder karma limit. "
-                                                         f"Link to their awarder logs {link}")
-            except requests.exceptions.HTTPError:
-                print(f"{comment.author.name} has reached awarder karma limit")
+                common_functions.send_message_to_discord(mod_channel_webhook, f"{comment.author.name} has reached awarder karma limit. "
+                                                                              f"Link to their awarder logs {link}")
+            except requests.exceptions.HTTPError as http_err:
+                db_manager_logger.warning(f"Message sending to Discord failed. {http_err}.")
             return CONSTANTS.KARMA_AWARDING_LIMIT_REACHED
 
     # checks rest of the conversation rules
-    return conversation_checks.checks_for_karma_command(comment, fallout76marketplace)
+    conv_check_result = conversation_checks.checks_for_karma_command(comment, fallout76marketplace)
+    db_manager_logger.info(f"Result for the conversation check {conv_check_result}")
+    return conv_check_result
 
 
 def insert_karma_log(db_conn, comment):
@@ -148,6 +143,7 @@ def process_command_non_mod_user(comment, fallout76marketplace, legacy76, db_con
     """
     comment_body = comment.body.strip().replace("\\", "")
     if re.search(CONSTANTS.KARMA_PP, comment_body, re.IGNORECASE):
+        db_manager_logger.info(f"Received karma plus comment (id={comment.id}) from u/{comment.author.name}.")
         output = karma_plus_command_non_mod_users(comment, fallout76marketplace, legacy76, db_conn, mod_channel_webhook)
         if output is CONSTANTS.KARMA_CHECKS_PASSED:
             insert_karma_log(db_conn, comment)
@@ -157,9 +153,11 @@ def process_command_non_mod_user(comment, fallout76marketplace, legacy76, db_con
             bot_responses.karma_rewarded_comment(comment)
     # If comment says Karma--
     elif re.search(CONSTANTS.KARMA_MM, comment_body, re.IGNORECASE):
+        db_manager_logger.info(f"Received karma minus comment (id={comment.id}) from u/{comment.author.name}.")
         bot_responses.karma_subtract_failed(comment)
     # Close submission
     elif re.search(CONSTANTS.CLOSE, comment_body, re.IGNORECASE):
+        db_manager_logger.info(f"Received close comment (id={comment.id}) from u/{comment.author.name}.")
         conversation_checks.checks_for_close_command(comment)
 
 
